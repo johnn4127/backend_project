dac5c9b4dc217439d6d53904310d26d4e72f83d2
johnn4127 <johnn4127@gmail.com>
2023-09-06 13:27:34 -0400

    adds login and registration with links to both

--- server.js
+++ server.js
@@ -1,22 +1,34 @@
-const express = require("express")
-const app = express()
-app.use(express.json()) 
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express();
+const winston = require("winston");
+const bcrypt = require('bcrypt')
+// const {} = require('./models')
+app.use(express.json())
+//link ejs/css
+app.use(express.static(__dirname + '/public'));
+const path = require('path')
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }))
 
-
-app.get('/register',(req,res) => {
-    
+app.get('/registration',(req,res) => {
+    res.render("registration")
 })
 
+app.get('/',(req,res) => {
+    res.send('welcome')
+})
 
 
 app.post('/register',(req,res)=>{
 })
 
 app.get('/login',(req,res) =>{
-
+    res.render("login")
 })
 
 app.post('/login',(req,res)=>{
+    res.render("login")
 })
 
 app.listen(3000, () =>{
