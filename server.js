e086412cea438b5ded7f0713ce29c5ba2b85040c
Deron <fambroderon@gmail.com>
2023-09-07 13:47:33 -0400

    finished setting up endpoints and links for password reset

--- server.js
+++ server.js
@@ -86,12 +86,18 @@ app.post('/login',(req,res)=>{
     res.render("login")
 })
 
+
+
+
 let user={
   id: "2",
   email: "theman@gmail.com" ,
   password: "lilbro"
 }
 const JWT_SECRET='some super secret...'
+
+
+
 app.get('/forgot_password',(req,res,next) =>{
   res.render("forgot_password")
 })
@@ -100,29 +106,69 @@ app.post('/forgot_password',(req,res,next)=>{
   const {email}=req.body;
 //makes sure user exist in database
 if(email !== user.email){
-
   res.send('User not found')
   return;
 }
-//creates a link for the existing user
-const secret=JWT_SECRET + user.password
+//creates a one time link for the user
+const secret=JWT_SECRET + user.password 
+//stored in the jwt token
 const payload={
   email: user.email,
   id: user.id
 }
+//creating the token and making it expire in 15 min
 const token=jwt.sign(payload,secret,{expiresIn: '15m'})
-const link=`http://localhost:3000/reset-password/${user.id}/${token}`
-console.log(link)
-res.send(`password reset link has been sent to ur email`)
+const link=`http://localhost:3000/reset-password/${user.id}/${token}` //where the user goes when they click the link
+console.log(link) //send emails here
+res.send(`password reset link has been sent to your email`)
 })
 
+
+
+//this is the rounte the link above takes the user to
 app.get('/reset-password/:id/:token',(req,res,next)=>{
 const {id,token}=req.params
-res.send(req.params)
-})
-app.post('/reset-password',(req,res,next)=>{
 
+//this checks if the id is in the database
+if(id !== user.id){
+  res.send(`invalid id`)
+  return
+}
+const secret=JWT_SECRET + user.password
+try{
+  const payload=jwt.verify(token, secret)
+  res.render('reset-password',{email: user.email})
+}catch(err){
+  console.log(err.message)
+  res.send(err.message)
+}
 })
+
+
+app.post('/reset-password/:id/:token',(req,res,next)=>{
+  const {id,token}=req.params
+  const {password,password2}=req.body
+  res.send(user)
+  if(id !== user.id){
+    res.send(`invalid id`)
+    return
+  }
+  const secret=JWT_SECRET + user.password
+  //validate password and passwoed2 should match
+  try{
+    const payload=jwt.verify(token, secret)
+    user.password=password
+    res.send(user)
+  }catch(err){
+    console.log(err.message)
+    res.send(err.message)
+  }
+})
+
+
+
+
+
 
 app.listen(3000, () =>{
     console.log(`Server is running on port 3000`)
