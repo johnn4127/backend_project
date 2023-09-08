64deb8ad3c82d782cc807d1c59e116a0ce6b73a3
Deron <fambroderon@gmail.com>
2023-09-08 11:12:11 -0400

    attempting merge

--- server.js
+++ server.js
@@ -223,149 +223,149 @@ app.post('/login', async (req, res) => {
 
 
 
-// // let user={
-// //   id: "2",
-// //   email: "theman@gmail.com" ,
-// //   password: "lilbro"
-// // }
-// const JWT_SECRET='some super secret...'
+// let user={
+//   id: "2",
+//   email: "theman@gmail.com" ,
+//   password: "lilbro"
+// }
+const JWT_SECRET='some super secret...'
+const message={
+  to: 'deronfambro0112@gmail.com',
+  from: 'snugglereads@gmail.com',
+  subject: 'hello from snugglereads',
+  text: 'hello from sendgrid',
+  html: '<h1>Hello from Snugglereads</h1>'
+}
+// sgMail.send(message).then(res=>console.log('email sent')).catch(err=>console.log(err.message))
+
+
+
+app.get('/forgot_password',(req,res,next) =>{
+  res.render("forgot_password")
+})
+
+app.post('/forgot_password',async(req,res,next)=>{
+  const {email}=req.body;
+//makes sure user exist in database
+try{
+  const user = await Accounts.findOne({ where: { email } });
+
+    if (!user) {
+      return res.send('User not found');
+    }
+
+    //creates a one time link for the user
+    const secret=JWT_SECRET + user.password 
+    //stored in the jwt token
+    const payload={
+      email: user.email,
+      id: user.id
+    }
+    // if(email !== user.email){
+    //   res.send('User not found')      //(test line!!!)
+    //   return;
+    // }
+//creating the token and making it expire in 15 min
+const token=jwt.sign(payload,secret,{expiresIn: '15m'})
+const link=`http://localhost:3000/reset-password/${user.id}/${token}` //where the user goes when they click the link
+// console.log(link) //send emails here
+const message = {
+  to: user.email,
+  from: 'snugglereads@gmail.com',
+  subject: 'Password Reset',
+  text: 'Click the link to reset your password: ' + link,
+  html: `<p>Click the link to reset your password:</p><a href="${link}">${link}</a>`,
+};
+
+await sgMail.send(message).then(() => {
+  console.log('Email Sent')
+  res.send(`password reset link has been sent to your email`)
+}).catch((err) => {
+  console.error(err.message);
+  res.send(err.message);
+});
+} catch (err) {
+console.error(err.message);
+res.send(err.message);
+}
+});
+
+
+//this is the rounte the link above takes the user to
+app.get('/reset-password/:id/:token', async(req,res,next)=>{
+const {id,token}=req.params
+try{
+  const user=await Accounts.findOne({ where: { id } })
+  //this checks if the id is in the database
+if(!user){
+  res.send(`invalid id`)
+  return
+}
+const secret=JWT_SECRET + user.password
+const payload=jwt.verify(token, secret)
+res.render('reset-password',{email: user.email})
+}catch(err){
+  console.log(err)
+  res.send()
+}
+})
+
+
+app.post('/reset-password/:id/:token',async(req,res,next)=>{
+  const {id,token}=req.params
+  const {password,password2}=req.body
+  try {
+    const user = await Accounts.findOne({ where: { id } });
+  // res.send(user)
+  const secret=JWT_SECRET + user.password
+  //validate password and passwoed2 should match
+  const payload = jwt.verify(token, secret);
+  
+  if(!user){
+    return res.send(`invalid id`)
+  }
+  if(password !== password2){
+    return res.send('passwords do not match')
+  }
+  // Update the user's password in the database
+  user.password = password;
+  await user.save();
+
+  res.send('Password reset successful');
+} catch (err) {
+  console.error(err.message);
+  res.send('password reset failed');
+}
+});
+  
+  
+
+//   try{
+//     const payload=jwt.verify(token, secret)
+//     user.password=password
+//     res.send(user)
+//   }catch(err){
+//     console.log(err.message)
+//     res.send(err.message)
+//   }
+// })
+
+
+// const sgMail=require('@sendgrid/mail')
+// const API_KEY='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'
+// sgMail.setApiKey(API_KEY)
+
 // const message={
-//   to: 'deronfambro0112@gmail.com',
-//   from: 'snugglereads@gmail.com',
-//   subject: 'hello from snugglereads',
-//   text: 'hello from sendgrid',
-//   html: '<h1>Hello from Snugglereads</h1>'
-// }
-// // sgMail.send(message).then(res=>console.log('email sent')).catch(err=>console.log(err.message))
-
-
-
-// app.get('/forgot_password',(req,res,next) =>{
-//   res.render("forgot_password")
-// })
-
-// app.post('/forgot_password',async(req,res,next)=>{
-//   const {email}=req.body;
-// //makes sure user exist in database
-// try{
-//   const user = await Accounts.findOne({ where: { email } });
-
-//     if (!user) {
-//       return res.send('User not found');
-//     }
-
-//     //creates a one time link for the user
-//     const secret=JWT_SECRET + user.password 
-//     //stored in the jwt token
-//     const payload={
-//       email: user.email,
-//       id: user.id
-//     }
-//     // if(email !== user.email){
-//     //   res.send('User not found')      //(test line!!!)
-//     //   return;
-//     // }
-// //creating the token and making it expire in 15 min
-// const token=jwt.sign(payload,secret,{expiresIn: '15m'})
-// const link=`http://localhost:3000/reset-password/${user.id}/${token}` //where the user goes when they click the link
-// // console.log(link) //send emails here
-// const message = {
-//   to: user.email,
-//   from: 'snugglereads@gmail.com',
-//   subject: 'Password Reset',
-//   text: 'Click the link to reset your password: ' + link,
-//   html: `<p>Click the link to reset your password:</p><a href="${link}">${link}</a>`,
-// };
-
-// await sgMail.send(message).then(() => {
-//   console.log('Email Sent')
-//   res.send(`password reset link has been sent to your email`)
-// }).catch((err) => {
-//   console.error(err.message);
-//   res.send(err.message);
-// });
-// } catch (err) {
-// console.error(err.message);
-// res.send(err.message);
-// }
-// });
-
-
-// //this is the rounte the link above takes the user to
-// app.get('/reset-password/:id/:token', async(req,res,next)=>{
-// const {id,token}=req.params
-// try{
-//   const user=await Accounts.findOne({ where: { id } })
-//   //this checks if the id is in the database
-// if(!user){
-//   res.send(`invalid id`)
-//   return
-// }
-// const secret=JWT_SECRET + user.password
-// const payload=jwt.verify(token, secret)
-// res.render('reset-password',{email: user.email})
-// }catch(err){
-//   console.log(err.message)
-//   res.send(err.message)
-// }
-// })
-
-
-// app.post('/reset-password/:id/:token',async(req,res,next)=>{
-//   const {id,token}=req.params
-//   const {password,password2}=req.body
-//   try {
-//     const user = await Accounts.findOne({ where: { id } });
-//   // res.send(user)
-//   const secret=JWT_SECRET + user.password
-//   //validate password and passwoed2 should match
-//   const payload = jwt.verify(token, secret);
-  
-//   if(!user){
-//     return res.send(`invalid id`)
+//     to: 'deronfambro0112@gmail.com',
+//     from: 'snugglereads@gmail.com',
+//     subject: 'hello from snugglereads',
+//     text: 'hello from sendgrid',
+//     html: '<h1>Hello from Snugglereads</h1>'
 //   }
-//   if(password !== password2){
-//     return res.send('passwords do not match')
-//   }
-//   // Update the user's password in the database
-//   user.password = password;
-//   await user.save();
-
-//   res.send('Password reset successful');
-// } catch (err) {
-//   console.error(err.message);
-//   res.send('password reset failed');
-// }
-// });
-  
-  
-
-// //   try{
-// //     const payload=jwt.verify(token, secret)
-// //     user.password=password
-// //     res.send(user)
-// //   }catch(err){
-// //     console.log(err.message)
-// //     res.send(err.message)
-// //   }
-// // })
-
-
-// // const sgMail=require('@sendgrid/mail')
-// // const API_KEY='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'
-// // sgMail.setApiKey(API_KEY)
-
-// // const message={
-// //     to: 'deronfambro0112@gmail.com',
-// //     from: 'snugglereads@gmail.com',
-// //     subject: 'hello from snugglereads',
-// //     text: 'hello from sendgrid',
-// //     html: '<h1>Hello from Snugglereads</h1>'
-// //   }
-// //   sgMail
-// //   .send(message)
-// //   .then(res=>console.log('email sent'))
-// //   .catch(error=>console.log(error.message))
+//   sgMail
+//   .send(message)
+//   .then(res=>console.log('email sent'))
+//   .catch(error=>console.log(error.message))
 
 
 
